9f2966385d25afe28b5045c2436b72d3ee5f9199
knosseir <kimo.nosseir@gmail.com>
2017-04-01 21:44:21 -0700

    Fixed formatting, links/videos sent now open in Chrome Custom Tabs

--- app/src/main/java/com/example/admin/chatapp/AboutActivity.java
+++ app/src/main/java/com/example/admin/chatapp/AboutActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 public class AboutActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
